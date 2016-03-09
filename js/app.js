ff7b5c4d6ff5282e1602813381d48d212b08fe86
josygarciac <j_osy94@hotmail.com>
2016-03-08 21:37:32 -0600

    Archivos iniciales

--- js/app.js
+++ js/app.js
@@ -1,19 +1,43 @@
-angular.module('todo', [
-    'ngRoute',
-    'persistencia',
-    'todo.controllers'
-]).config(['$routeProvider', function($routeProvider) {
-    $routeProvider
-        .when('/indice', {
-            templateUrl: 'html/indice.html',
-            controller: 'IndiceController'
-        })
-        .when('/detalle/:id', {
-            templateUrl: 'html/detalle.html',
-            controller: 'DetalleController'
-        })
-        .otherwise({redirectTo: '/indice'});
-}])
-;
+(function() {
+  'use strict';
 
-angular.module('todo.controllers', []);
+  angular /*Setter*/
+    .module('myBank', ['ui.router','ngStorage'])
+    .config(['$stateProvider', '$urlRouterProvider', config])
+    .controller('PrincipalCtrl', PrincipalCtrl);
+
+  function config($stateProvider, $urlRouterProvider) {
+
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'views/registro/registro.view.html',
+        controller: 'RegistroController',
+        controllerAs: 'registro'
+      })
+      .state('detalle', {
+        url: '/detalle/:id',
+        templateUrl: 'views/detalle/detalle.view.html',
+        controller: 'DetalleController',
+        controllerAs: 'detalle'
+      })
+      .state('crear', {
+        url: '/crear',
+        templateUrl: 'views/crear/crear.view.html',
+        controller: 'CrearController',
+        controllerAs: 'crear'
+      })
+      .state('agregar', {
+        url: '/agregar',
+        templateUrl: 'views/agregar/agregar.view.html',
+        controller: 'AgregarController',
+        controllerAs: 'agregar'
+      });
+      
+      $urlRouterProvider.otherwise('/');
+  }
+
+  function PrincipalCtrl() {
+    
+  }
+})();
